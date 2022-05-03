b60f41418f7912e5703f38f69c22105b31cb8cf9
Camila <camilacassimiro@gmail.com>
2022-05-03 12:20:19 -0300

    first commit

---
+++ exercicio01.js
@@ -0,0 +1,4 @@
+var exampleLet = 31;
+var exampleConst = "Camila";
+var exampleVar = "Olá! Meu nome é " + exampleConst + " e tenho " + exampleLet + " anos.";
+console.log(exampleVar);
---
+++ exercicio01.ts
@@ -0,0 +1,5 @@
+let exampleLet:number = 31;
+const exampleConst:string = "Camila";
+var exampleVar:string = "Olá! Meu nome é " + exampleConst + " e tenho " + exampleLet + " anos."
+
+console.log(exampleVar)
