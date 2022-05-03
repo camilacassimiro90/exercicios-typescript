8c62dbbb1a4077c55d504bc8044a3e4dc34d3bec
Camila <camilacassimiro@gmail.com>
2022-05-03 12:55:45 -0300

    adicionei exercicio 03

---
+++ exercicio03.js
@@ -0,0 +1,17 @@
+// let aluno:{
+//   nome: string,
+//   matricula: number,
+//   ativo: boolean,
+//   cadeiras: string[] 
+// };
+// aluno.nome = "Camila";
+// aluno.matricula = 123456;
+// aluno.ativo = true;
+// aluno.cadeiras = ["Github", "Javascript", "SASS", "Typescript", "Angular"];
+// console.log(aluno)
+var aluno;
+// aluno.nome = 'Edmilson';
+aluno.matricula = 800188;
+aluno.ativo = true;
+aluno.cadeiras = ["JavaScript", "Angular", "C#", ".Net Core", "Scrum"];
+console.log(aluno);
---
+++ exercicio03.ts
@@ -0,0 +1,13 @@
+let aluno:{
+  nome: string,
+  matricula: number,
+  ativo: boolean,
+  cadeiras: string[] 
+};
+aluno.nome = "Camila";
+aluno.matricula = 123456;
+aluno.ativo = true;
+aluno.cadeiras = ["Github", "Javascript", "SASS", "Typescript", "Angular"];
+
+console.log(aluno)
+
